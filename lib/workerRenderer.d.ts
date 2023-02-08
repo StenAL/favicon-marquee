9775f32211eab2c567948a717e69a3e1a9d377f7
Sten Laane <21343173+StenAL@users.noreply.github.com>
2023-02-08 08:48:37 +0000

    Stop commiting build output to version control
    This just makes commits bigger than they need to be and increases the
    work of handling merge conflicts without any benefits to show for it.

--- lib/workerRenderer.d.ts
+++
@@ -1,9 +0,0 @@
-import { FaviconMarqueeParameters, Renderer } from "./types.js";
-export declare class WorkerRenderer implements Renderer {
-    private readonly favicon;
-    private readonly parameters;
-    private readonly worker;
-    constructor(parameters: FaviconMarqueeParameters, favicon: HTMLLinkElement);
-    start(): void;
-    stop(): void;
-}
