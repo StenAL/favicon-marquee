d4f76614a36af6147c060c68abf079046d393933
Sten Laane <21343173+StenAL@users.noreply.github.com>
2023-02-08 21:36:15 +0000

    Make E2E test runnable using a remote WebDriver server
    Also add a docker-compose setup that makes use of this. The E2E test can
    now be run either in Docker or locally.

--- src/__tests__/e2e.ts
+++ src/__tests__/e2e.ts
@@ -1,12 +1,32 @@
-import { Browser, Builder, By } from "selenium-webdriver";
+import { Browser, Builder, By, WebDriver } from "selenium-webdriver";
 import { setTimeout } from "timers/promises";
 import { resolve } from "path";
 
-const FILE_URL = resolve(process.cwd(), "examples", "example1.html");
+const LOCAL_FILE_PATH = resolve(process.cwd(), "examples", "example1.html");
+const CONTAINER_FILE_PATH = "/files/examples/example1.html";
+const DRIVER_CONNECTION_RETRIES = 3;
+const runningInContainer = Boolean(process.env.SELENIUM_REMOTE_URL);
+
+const createWebDriver = async (): Promise<WebDriver> => {
+    for (let attempt = 1; attempt <= DRIVER_CONNECTION_RETRIES + 1; attempt++) {
+        try {
+            const driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+            console.log(`Successfully connected to driver on attempt ${attempt}!`);
+            return driver;
+        } catch (e) {
+            const message = typeof e === "object" && e !== null && "message" in e ? e.message : undefined;
+            console.log(`Failed to connect to  WebDriver on attempt ${attempt}${message ? `: ${message}` : undefined}`);
+            await setTimeout(1000);
+        }
+    }
+    throw new Error("Could not connect to WebDriver");
+};
+
 const run = async () => {
     console.log("Initializing...");
-    const driver = await new Builder().forBrowser(Browser.FIREFOX).build();
-    await driver.get(`file://${FILE_URL}`);
+    const driver = await createWebDriver();
+    const filePath = runningInContainer ? CONTAINER_FILE_PATH : LOCAL_FILE_PATH;
+    await driver.get(`file://${filePath}`);
     console.log("Website loaded!");
     await driver.findElement(By.id("stop-marquee")).click();
     await driver.findElement(By.id("start-marquee")).click();
@@ -20,6 +40,6 @@ const run = async () => {
 };
 
 run().catch((e) => {
-    console.log(e);
+    console.error(e);
     process.exit(1);
 });
