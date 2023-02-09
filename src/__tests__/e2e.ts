90cd0476db85bacdcf1b784ce451e2345070d983
Sten Laane <21343173+StenAL@users.noreply.github.com>
2023-02-09 09:10:46 +0000

    Add video recording to E2E test
    This generates an mp4 file that shows FaviconMarquee working, similar to
    the example gifs in the examples directory.

--- src/__tests__/e2e.ts
+++ src/__tests__/e2e.ts
@@ -1,10 +1,15 @@
 import { Browser, Builder, By, WebDriver } from "selenium-webdriver";
 import { setTimeout } from "timers/promises";
 import { resolve } from "path";
+import { unlink, writeFile } from "node:fs/promises";
 
 const LOCAL_FILE_PATH = resolve(process.cwd(), "examples", "example1.html");
 const CONTAINER_FILE_PATH = "/files/examples/example1.html";
+const HEALTHCHECK_FILE = "/tmp/favicon-marquee-node-container-ready";
+
 const DRIVER_CONNECTION_RETRIES = 3;
+const RECORDING_WAIT_RETRIES = 5;
+
 const runningInContainer = Boolean(process.env.SELENIUM_REMOTE_URL);
 
 const createWebDriver = async (): Promise<WebDriver> => {
@@ -22,6 +27,26 @@ const createWebDriver = async (): Promise<WebDriver> => {
     throw new Error("Could not connect to WebDriver");
 };
 
+const waitForRecording = async () => {
+    for (let attempt = 1; attempt <= RECORDING_WAIT_RETRIES + 1; attempt++) {
+        try {
+            await fetch("http://firefox_recording:9000");
+            console.log(`video is recording on attempt ${attempt}!`);
+            return;
+        } catch (e) {
+            const message = typeof e === "object" && e !== null && "message" in e ? e.message : undefined;
+            console.log(`Video not recording yet, attempt ${attempt}${message ? ` (${message})` : undefined}`);
+            await setTimeout(1000);
+        }
+    }
+    throw new Error("Video recording container did not start");
+};
+
+const signalReady = async () => {
+    console.log(`Creating healthcheck file at ${HEALTHCHECK_FILE}`);
+    await writeFile(HEALTHCHECK_FILE, "");
+};
+
 const run = async () => {
     console.log("Initializing...");
     const driver = await createWebDriver();
@@ -29,6 +54,11 @@ const run = async () => {
     await driver.get(`file://${filePath}`);
     console.log("Website loaded!");
     await driver.findElement(By.id("stop-marquee")).click();
+    if (runningInContainer) {
+        await signalReady();
+        console.log("Waiting for recording container to start");
+        await waitForRecording();
+    }
     await driver.findElement(By.id("start-marquee")).click();
     try {
         console.log("Waiting 5 seconds");
@@ -36,6 +66,9 @@ const run = async () => {
     } finally {
         console.log("Teardown...");
         await driver.quit();
+        if (runningInContainer) {
+            await unlink(HEALTHCHECK_FILE);
+        }
     }
 };
 
